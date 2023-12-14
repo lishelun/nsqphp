cc27771831a6ee06ef95888604c42e97b5975d02
Lee <lishelun@qq.com>
2023-12-14 19:35:29 +0800

    Update Response.php
    兼容有赞NSQ

--- src/Lookup/Response.php
+++ src/Lookup/Response.php
@@ -13,22 +13,30 @@ final class Response
      * @param Producer[] $producers
      */
     public function __construct(
-        public array $channels,
-        public array $producers,
-    ) {
+        public array $channels ,
+        public array $producers ,
+    )
+    {
     }
 
     public static function fromJson(string $json): self
     {
-        $array = json_decode($json, true, flags: JSON_THROW_ON_ERROR);
+        $array = json_decode($json , true , flags: JSON_THROW_ON_ERROR);
 
-        if (\array_key_exists('message', $array)) {
+        if ( \array_key_exists('message' , $array) ) {
             throw new LookupException($array['message']);
+        }
+        //兼容有赞nsqlookupd升级版
+        if ( \array_key_exists('status_code' , $array) && \array_key_exists('data' , $array) ) {
+            return new self(
+                $array['data']['channels'] ?? [] ,
+                array_map([Producer::class , 'fromArray'] , $array['data']['producers']) ,
+            );
         }
 
         return new self(
-            $array['channels'] ?? [],
-            array_map([Producer::class, 'fromArray'], $array['producers']),
+            $array['channels'] ?? [] ,
+            array_map([Producer::class , 'fromArray'] , $array['producers']) ,
         );
     }
 }
